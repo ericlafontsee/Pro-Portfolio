8cea8489e1b211057e1deb26b97e78254b7ac2e5
Eric LaFontsee <elafontsee@gmail.com>
2021-03-08 08:32:51 -0600

    Added a different contact form animation

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,94 +1,98 @@
-// import 'antd/dist/antd.css'
-import "./style.css";
-import React, { Fragment, useState } from "react";
-import { Keyframes, animated } from "react-spring/renderprops";
-import { Form, Input, Button } from "antd";
-import delay from "delay";
-
-// Creates a spring with predefined animation slots
-const Sidebar = Keyframes.Spring({
-  // Slots can take arrays/chains,
-  peek: [
-    { x: 0, from: { x: -100 }, delay: 500 },
-    { x: -100, delay: 800 }
-  ],
-  // single items,
-  open: { delay: 0, x: 0 },
-  // or async functions with side-effects
-  close: async (call) => {
-    await delay(400);
-    await call({ delay: 0, x: -100 });
-  }
-});
-
-// Creates a keyframed trail
-const Content = Keyframes.Trail({
-  peek: [
-    { x: 0, opacity: 1, from: { x: -100, opacity: 0 }, delay: 600 },
-    { x: -100, opacity: 0, delay: 0 }
-  ],
-  open: { x: 0, opacity: 1, delay: 100 },
-  close: { x: -100, opacity: 0, delay: 0 }
-});
+import {config } from 'react-spring';
+import delay from 'delay';
+import ReactPlaceholder from 'react-placeholder';
+import React, { useState } from "react";
+import { Keyframes } from "react-spring/renderprops";
+import './style.css';
 
 const items = [
-  <Input size="medium" placeholder="Name" />,
-  <Input size="medium" placeholder="Phone" />,
-  <Input size="medium" placeholder="email" />,
-  <Input size="medium" placeholder="Subject" />,
-  <Input size="large" placeholder="Message" />,
-  <Fragment>
-    <Button
-      size="small"
-      type="primary"
-      htmlType="submit"
-      className="login-form-button"
-      children="Log in"
-    />
-  </Fragment>
+  {
+    key: 1,
+    content: <ReactPlaceholder type="media" rows={2} />,
+  },
+  {
+    key: 2,
+    content: 'Home',
+  },
+  {
+    key: 3,
+    content: 'Favourites',
+  },
+  {
+    key: 4,
+    content: 'Sign out',
+  },
 ];
 
-function Contact() {
-  const [open, setOpen] = useState(false);
-  const toggle = () => setOpen(!open);
+const Menu = Keyframes.Spring({
+  in: async next => {
+    await next({
+      transform: 'translateX(0%)',
+    });
+  },
+  out: async next => {
+    await delay(700);
+    await next({
+      transform: 'translateX(-100%)',
+    });
+  },
+});
 
-  const state =
-    open === false ? "peek" : setOpen(true) ? "open" : "close";
-  const icon = open ? "fold" : "unfold";
+const MenuItems = Keyframes.Trail({
+  in: async next => {
+    await delay(600);
+    await next({
+      opacity: 1,
+      transform: 'translateX(0px)',
+    });
+  },
+  out: async next => {
+    await next({
+      opacity: 0,
+      transform: 'translateX(-40px)',
+    });
+  },
+});
+
+function Contact() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+  const handleBtnClick = () => {
+setMenuOpen(!menuOpen);
+     
+  };
+
+
   return (
-    <div style={{ background: "lightblue", width: "100%", height: "100%" }}>
-      <Sidebar native state={state}>
-        {({ x }) => (
-          <animated.div
-            className="sidebar"
-            style={{
-              transform: x.interpolate((x) => `translate3d(${x}%,0,0)`)
-            }}
-          >
-            <Content
-              native
-              items={items}
-              keys={items.map((_, i) => i)}
-              reverse={!open}
-              state={state}
-            >
-              {(item, i) => ({ x, ...props }) => (
-                <animated.div
-                  style={{
-                    transform: x.interpolate((x) => `translate3d(${x}%,0,0)`),
-                    ...props
-                  }}
+      <>
+      <div className="App">
+        <button onClick={handleBtnClick}>☰</button>
+
+        <Menu
+          config={config.gentle}
+          unique
+          state={menuOpen ? 'in' : 'out'}
+        >
+          {props => (
+            <nav style={props}>
+              <ul>
+                <MenuItems
+                  keys={item => item.key}
+                  items={items}
+                  state={menuOpen ? 'in' : 'out'}
+                  reverse={menuOpen}
                 >
-                  <Form.Item className={i === 0 ? "middle" : ""}>
-                    {item}
-                  </Form.Item>
-                </animated.div>
-              )}
-            </Content>
-          </animated.div>
-        )}
-      </Sidebar>
-    </div>
+                  {trailitem => trailprops => (
+                    <li style={trailprops}>{trailitem.content}</li>
+                  )}
+                </MenuItems>
+              </ul>
+            </nav>
+          )}
+        </Menu>
+      </div>
+    </>
   );
 }
 
